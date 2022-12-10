832b652831206bcde57e2528ffd7629ca1866b6c
Sam Robinson <110217039+SamRobinson-2511@users.noreply.github.com>
2022-12-10 13:40:03 -0600

    commit tree 5d2efcb1ac4f5b710b41e746cec825ae618dcb1c

--- methods.rb
+++ methods.rb
@@ -1,1 +1,29 @@
 # Your code here!
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+greet_programmer
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+greet("Naureen")
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+greet_with_default "Sunny"
+
+def add(num1, num2)
+    return num1 + num2
+end
+add(1,3)
+
+def halve(num1)
+    if num1.class !=Integer
+        return nil
+    end
+    return num1/2
+end
+halve(2)
